c4c741bdbb13605b173276b19e0f4c1c0d1096c6
Sixto Velarde <163205281+sixtovelarde@users.noreply.github.com>
2024-07-23 16:23:58 -0400

    feat: add books function added

--- Bilioteca.cpp
+++ Bilioteca.cpp
@@ -29,6 +29,10 @@ struct libros{
 void LeerDatosLibros(const string &filename, libros catalog[], int &catalogsize);
 void LeerDatosPersonas(const string &filename, Persona catalog[], int &catalogsize);
 
+void AgregarDatosLibros();
+void EliminarDatosLibros();
+void ModificarDatosLibros();
+
 int main(){
 int status = 0;
 while(status!=1 and status!=2){
@@ -87,4 +91,31 @@ void LeerDatosPersonas(const string &filename, Persona catalog[], int &catalogsi
 
         catalogsize++;
     }
+}
+void AgregarDatosLibros(){
+    ofstream bookfile("Books.csv" , ios::app);
+    ifstream bookfile2("Books.csv");
+    string line;
+    libros l;
+    int new_id = 0;
+
+    while(getline(bookfile2,line)){
+        new_id++;
+    }
+    cout<<"Ingrese el titulo: ";
+    getline(cin,l.titulo);
+    cout<<"Ingrese el nombre del autor: ";
+    getline(cin,l.autor);
+    cout<<"Ingrese el genero: ";
+    getline(cin,l.genero);
+    cout<<"Ingrese la fecha de publicacion: ";
+    getline(cin,l.fecha_publicacion);
+    cout<<"Ingrese la editorial: ";
+    getline(cin,l.editorial);
+    cout<<"Ingrese el precio del libro: ";
+    cin>>l.precio;
+
+    bookfile<<l.id<<","<<l.titulo<<","<<l.autor<<","<<l.genero<<","<<l.fecha_publicacion<<","<<l.editorial<<","<<l.precio<<endl;
+    bookfile.close();
+    bookfile2.close();
 }
